3fef5e8e7f68e736997a6d1e0bf7a343b7700c8a
Olanrewaju Lawal <SN035984@firstbanknigeria.com>
2026-06-27 19:44:04 +0100

    done

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,6 @@ import {
 // RAG / Knowledge Base
 import kbUploadRoute from './mastra/core/rag/routes/upload.route.js';
 import kbDocsRoute from './mastra/core/rag/routes/docs.route.js';
-import feedbackSurveyRoute from './routes/feedback-survey.route.js';
 import { createKbDocsTable } from './mastra/core/rag/db.js';
 import { initVectorIndex } from './mastra/core/rag/vector-store.js';
 import { warmUpEmbeddingModel } from "./mastra/core/llm/provider.js";
@@ -82,41 +81,42 @@ const PORT =
 
 
 const URL =
-  process.env.LOCAL_URL?.replace(/\/$/, '') ||
+  process.env.REMOTE_URL?.replace(/\/$/, '') ||
   process.env.SERVER_URL?.replace(/\/$/, '');
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  const allowed = new Set([
-    'http://127.0.0.1:5500',
-    'http://localhost:5500',
-    'http://127.0.0.1:3000',
-    'http://localhost:3000',
-  ]);
+// app.use((req, res, next) => {
+//   const origin = req.headers.origin;
+//   const allowed = new Set([
+//     'http://127.0.0.1:5500',
+//     'http://localhost:5500',
+//     'http://127.0.0.1:3000',
+//     'http://localhost:3000',
+//   ]);
 
-  if (origin && allowed.has(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Vary', 'Origin');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
-  }
+//   if (origin && allowed.has(origin)) {
+//     res.setHeader('Access-Control-Allow-Origin', origin);
+//     res.setHeader('Vary', 'Origin');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
+//   }
 
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
-  }
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(204);
+//   }
 
-  next();
-});
+//   next();
+// });
 
 
 
 // Knowledge Base routes
+
+
 app.use('/api/kb/upload', kbUploadRoute);
 app.use('/api/kb/docs', kbDocsRoute);
-app.use('/admin/feedback-survey', feedbackSurveyRoute);
 
 app.use((req, res, next) => {
   res.setHeader('ngrok-skip-browser-warning', 'true');
