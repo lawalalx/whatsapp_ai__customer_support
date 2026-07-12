6b37e0b07155ec6e83ff18daf3c50c4bf2901361
Olanrewaju Lawal <SN035984@firstbanknigeria.com>
2026-07-12 23:04:35 +0100

    done

--- src/index.ts
+++ src/index.ts
@@ -82,7 +82,7 @@ const PORT =
 
 
 const URL =
-  process.env.LOCAL_URL?.replace(/\/$/, '') ||
+  process.env.REMOTE_URL?.replace(/\/$/, '') ||
   process.env.SERVER_URL?.replace(/\/$/, '');
 
 app.use(express.json());
